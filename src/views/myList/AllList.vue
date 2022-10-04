d87009bb16cef0b85a465c8bcbeba266b3aff4c0
AungKyawPhyo1142 <aungkyawphyo22233@gmail.com>
2022-10-04 13:10:47 +0630

    create list

---
+++ src/views/myList/AllList.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1 class="text-primary fs-1 text-uppercase">Todo List Page</h1>
+        <li class="list-group-item mx-5">
+            <single-list></single-list>
+            <single-list></single-list>
+        </li>
+    </div>
+</template>
+
+<script>
+    import singleList from './SingleList.vue';
+    export default {
+        name: 'AllList',
+        components: {
+            singleList
+        },
+    }
+</script>
---
+++ src/views/myList/SingleList.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="my-2">
+        <input  type="text" class="form-control"
+                    v-model="listText" @keyup.enter="saveText()"
+                    v-if="editable" >
+        <span v-else @dblclick="editText()">{{listText}}</span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'SingleList',
+        data () {
+            return {
+                listText: '',
+                editable: true
+            }
+        },
+        methods: {
+            saveText () {
+                this.editable = false;
+            },
+            editText(){
+                console.log('db click');
+                this.editable = true;
+            }
+
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
